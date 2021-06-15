30d32e320ed5678ad0b9836cf0b4ee9cf7b6f9f5
Riddhi Suteri <ridsuteri@gmail.com>
2021-06-15 07:18:41 +0530

    5 move all negative numbers to end (order not imp)

---
+++ Arrays/rearrange.java
@@ -0,0 +1,35 @@
+class rearrange {
+    
+    public void segregateElements(int arr[], int n)
+    {
+        int left = 0;
+        int right = n - 1;
+        
+        while(left <= right){
+            
+            if(arr[left] > 0 && arr[right] < 0){
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                
+                left++;
+                right--;
+            }
+            
+            else if(arr[left] < 0 && arr[right] < 0){
+                left++;
+            }
+            
+            else if(arr[left] > 0 && arr[right] > 0){
+                right --;
+            }
+            
+            else{
+                left++;
+                right--;
+            }
+        }
+        
+        
+    }
+}
