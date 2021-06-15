6d8ba6935ba11b0c49b2a1d896264496b73e481f
Riddhi Suteri <ridsuteri@gmail.com>
2021-06-15 18:17:00 +0530

    11 find duplicate - immutable

--- Arrays/findDuplicate.java
+++ Arrays/findDuplicate.java
@@ -1,5 +1,23 @@
 class findDuplicate {
-//     TO DO - FLYOD TORTOISE METHOD
+//     FLYOD TORTOISE METHOD - for immutable 
+     public int findDuplicate(int[] arr){
+        int slow = arr[0];
+		int fast = arr[0];
+
+		do {
+			slow = arr[slow];
+			fast = arr[arr[fast]];
+		} while (slow != fast);
+
+		fast = arr[0];
+
+		while (slow != fast) {
+			slow = arr[slow];
+			fast = arr[fast];
+		}
+        return slow;
+    }
+    
     public int findDuplicate(int[] nums) {
         int n =  nums.length;
         for(int i = 0; i < n; i++)
