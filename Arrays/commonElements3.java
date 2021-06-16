31c4de5927fee1c4fa81540d479393aaac273448
Riddhi Suteri <ridsuteri@gmail.com>
2021-06-16 06:48:52 +0530

    19 common elements from 3 sorted arrays

---
+++ Arrays/commonElements3.java
@@ -0,0 +1,24 @@
+class commonElements3{
+   ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
+    {
+        int i = 0, j = 0, k = 0;
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        int last = Integer.MIN_VALUE;
+
+        while (i < n1 && j < n2 && k < n3)
+        {
+            if (A[i] == B[j] && A[i] == C[k] && A[i] != last) 
+            {
+                res.add (A[i]);
+                last = A[i];
+                i++;
+                j++;
+                k++;
+            }
+            else if (Math.min (A[i], Math.min(B[j], C[k])) == A[i]) i++;
+            else if (Math.min (A[i], Math.min(B[j], C[k])) == B[j]) j++;
+            else k++;
+        }
+        return res;
+    }
+}
