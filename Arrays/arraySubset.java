57362cce0666343020887325a0c5804575239b89
Riddhi Suteri <ridsuteri@gmail.com>
2021-06-16 10:09:57 +0530

    28 array subset of another array

---
+++ Arrays/arraySubset.java
@@ -0,0 +1,15 @@
+class arraySubset {
+    public String isSubset( long a1[], long a2[], long n, long m) {
+        Set<Long> s = new HashSet<>();
+        
+        for(long i : a1)
+            s.add(i);
+            
+        for(long i : a2)
+            if(!s.contains(i))
+                return "No";
+        
+        return "Yes";
+        
+    }
+}
