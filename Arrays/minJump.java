4c879f14a11b1b229f02966aa391ac120e22553a
Riddhi Suteri <ridsuteri@gmail.com>
2021-06-24 10:52:43 +0530

    10 	Min no. of Jumps to reach end of an array - M

--- Arrays/minJump.java
+++ Arrays/minJump.java
@@ -1,48 +1,32 @@
 class minJump{
     static int minJumps(int[] arr){
-       if (arr.length <= 1)
-            return 0;
- 
-        // Return -1 if not possible to jump
-        if (arr[0] == 0)
+        // your code here
+        int n = arr.length;
+        int jumps = 1;
+        
+        if(n == 1 )
             return -1;
- 
-        // initialization
-        int maxReach = arr[0];
-        int step = arr[0];
-        int jump = 1;
- 
-        // Start traversing array
-        for (int i = 1; i < arr.length; i++) {
-            // Check if we have reached
-// the end of the array
-            if (i == arr.length - 1)
-                return jump;
- 
-            // updating maxReach
-            maxReach = Math.max(maxReach, i + arr[i]);
- 
-            // we use a step to get to the current index
-            step--;
- 
-            // If no further steps left
-            if (step == 0) {
-                // we must have used a jump
-                jump++;
- 
-                // Check if the current
-// index/position or lesser index
-                // is the maximum reach point
-// from the previous indexes
-                if (i >= maxReach)
-                    return -1;
- 
-                // re-initialize the steps to the amount
-                // of steps to reach maxReach from position i.
-                step = maxReach - i;
-            }
+            
+        if(arr[0] == 0)
+            return -1;
+            
+        int farthest = arr[0];
+        int curr_end = arr[0];
+        
+        for(int i = 1; i < n; i++){
+            if(i > farthest)
+                return -1;
+                
+            if(i == n - 1)
+                return jumps;
+                
+                farthest = Math.max(farthest, i + arr[i]);
+                
+                if(i == curr_end){
+                    jumps++;
+                    curr_end = farthest;
+                }
         }
- 
-        return -1;
+        return jumps;
     }
 }
