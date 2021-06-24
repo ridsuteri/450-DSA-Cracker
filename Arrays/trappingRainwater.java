d2cd4e84376a9c898c15d842854b14f4bb7d685e
Riddhi Suteri <ridsuteri@gmail.com>
2021-06-24 17:07:07 +0530

    29 Trapping Rain water problem

---
+++ Arrays/trappingRainwater.java
@@ -0,0 +1,23 @@
+class trappingRainwater{
+
+    static int trappingWater(int arr[], int n) { 
+        
+        int left[] = new int[n];
+        
+        int right[] = new int[n];
+        
+        left[0] = arr[0];
+        for (int i = 1;i < n;i++)
+            left[i] = Math.max(left[i - 1], arr[i]);
+        
+        right[n - 1] = arr[n - 1];
+        for(int i = n - 2;i >= 0;i--)
+            right[i] = Math.max(right[i + 1], arr[i]);
+        
+        int water = 0;
+        for (int i = 1;i < n - 1;i++)
+            water += Math.min(left[i], right[i]) - arr[i];
+        
+        return water;
+    } 
+}
