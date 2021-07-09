34ad921d7e620da5106ca3cf71924cf476d99bb7
Riddhi Suteri <ridsuteri@gmail.com>
2021-07-09 21:00:44 +0530

    2 Implement DFS Algo

---
+++ Graph/dfs.java
@@ -0,0 +1,26 @@
+public class dfs {
+    private LinkedList<Integer> adjLists[];
+  private boolean visited[];
+
+  // Graph creation
+  dfs(int vertices) {
+    adjLists = new LinkedList[vertices];
+    visited = new boolean[vertices];
+
+    for (int i = 0; i < vertices; i++)
+      adjLists[i] = new LinkedList<Integer>();
+  }
+
+  // DFS algorithm
+  void DFS(int vertex) {
+    visited[vertex] = true;
+    System.out.print(vertex + " ");
+
+    Iterator<Integer> ite = adjLists[vertex].listIterator();
+    while (ite.hasNext()) {
+      int adj = ite.next();
+      if (!visited[adj])
+        DFS(adj);
+    }
+  }
+}
