009c162e5e16650e2c11e28fb2effe6e59d76e59
Nikhil-69 <83407509+Nikhil-69@users.noreply.github.com>
2021-10-30 21:44:59 +0530

    2 height of a tree

---
+++ Binary Trees/height.java
@@ -0,0 +1,21 @@
+class height{
+	
+   int maxDepth(Node node)
+    {
+        if (node == null)
+            return -1;
+        else
+        {
+            /* compute the depth of each subtree */
+            int lDepth = maxDepth(node.left);
+            int rDepth = maxDepth(node.right);
+  
+            /* use the larger one */
+            if (lDepth > rDepth)
+                return (lDepth + 1);
+             else
+                return (rDepth + 1);
+        }
+    }
+
+}
